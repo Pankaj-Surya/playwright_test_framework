43e96714ada8b189c2821db4d300a36b1b9379fd
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2025-01-01 15:42:38 +0530

    added credconfig

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,23 +1,27 @@
-class LoginPage{
-    constructor(page){
-       this.page = page 
-      // this.page.waitForLoadState();
-       this.username =  page.locator("//input[@id='userEmail']");
-       this.password = page.locator("#userPassword")
-       this.signInButton = page.locator("[value='Login']");
+class LoginPage {
+    constructor(page) {
+        this.page = page
+        // this.page.waitForLoadState();
+        this.username = page.locator("//input[@id='userEmail']");
+        this.password = page.locator("#userPassword")
+        this.signInButton = page.locator("[value='Login']");
         page.waitForLoadState('networkidle');
     }
 
-    async goto(){
-         await this.page.goto("https://rahulshettyacademy.com/client");
+    async goto() {
+        await this.page.goto("https://rahulshettyacademy.com/client");
     }
 
-    async login(us,ps){
-       await this.username.fill(us)
+    async login(us, ps) {
+        if (!us || !ps) {
+            throw new Error('Username or password is missing');
+        }
+
+        await this.username.fill(us)
         await this.password.fill(ps)
         await this.signInButton.click()
         await this.page.waitForLoadState('networkidle');
     }
 }
 
-module.exports = {LoginPage}
+module.exports = { LoginPage }
--- tests/LoginTest.spec.js
+++ tests/LoginTest.spec.js
@@ -1,26 +1,25 @@
 const { test, expect } = require('@playwright/test');
 const BrowserManager = require('../browser/browserManager')
 const {POManager} = require('../pages/POManager');
+const {email, password} = require('../resources/CredentialConfig.js')
 
-// const { page } = require('./BaseTest.js'); // Import the shared page object
 const { getPage } = require('./BaseTest.js'); // Import the page from BaseTest
-test.describe('Test with Browser Manager', () => {
+test.describe('Login Test Suite', () => {
   // let browserManager, page;
  
-
   // test.beforeEach(async () => {
   //   browserManager = new BrowserManager();
   //   page = await browserManager.getBrowserPage('chrome', '');
   // });
 
-//   test.afterEach(async () => {
-//     awa;
-//   });
 
   test('Login Test', async () => {
-    const email = "pankaj1998@gmail.com";
-    const password ="Table@123"
+
     const page = await getPage(); // Access the page instance from BaseTest
+  
+    if (!email || !password) {
+      throw new Error("Email or password is missing");
+    }
     
     // POManageer
     const poManager = new POManager(page);
